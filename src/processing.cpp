d428ede18d23a7fca04eb0121a838a6ee1f528d0
Haotian Shi <shihaotian1@outlook.com>
2022-02-19 13:43:16 +0000

    pro

--- src/processing.cpp
+++ src/processing.cpp
@@ -1,11 +1,14 @@
-#include "processing.h"
+#include "processing.h":q
+#include "record.h"
+
 
 void processing(char *buffer) {
     while(true){
-
+        int *buffertemp;
+        buffertemp = buffer;
         int data = *buffer;
         printf("%d", data);
-    
+        
     }   
 }
 
