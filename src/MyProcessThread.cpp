c4cdcef050f7e7b0eb30d7b81d39f2c2b2a06ed5
LancerMar <602545140@qq.com>
2022-02-26 04:42:32 +0000

    test FFTW

--- src/MyProcessThread.cpp
+++ src/MyProcessThread.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <fftw3.h>
+#include <fstream>
 
 void MyProcessThread::run(){
     //for test
@@ -11,9 +12,18 @@ void MyProcessThread::run(){
 void MyProcessThread::testRun(){
     //std::cout<<" this is just for debuging the run functions"<< std::endl;
 
-    //fftw_complex *record_data;
-    //fftw_complex *dft_out_data;
+    fftw_complex *record_data;
+    fftw_complex *dft_out_data;
+    fftw_plan fft_processor;
 
-    
+    int N=1000;
+    record_data = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*N);
+    std::ifstream file("testdata.dat");
+
+    for(int i=0;i<N;i++)
+    {
+        record_data[i][0]=file.get
+    }
+
 
 }
---
+++ src/test.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <fftw3.h>
+
+int main()
+{
+	std::ofstream ofout("out.dat");
+
+	ofout.precision(4);
+    
+    fftw_complex *in;
+    fftw_complex *out;
+    fftw_plan p;
+
+    int N=3000;
+    in = (fftw_complex*)fftw_malloc(sizeof(float)*N);
+    out = (fftw_complex*)fftw_malloc(sizeof(float)*N);
+    std::ifstream file("testdata.dat",std::ios::in | std::ios::binary);
+
+    char val[4*N];
+    char* val_tmp;
+    val_tmp=val;
+    for (int i=0;i<N*4;i=i+4)
+    {
+        file.read(val_tmp,4);
+        in[i/4][0] =*(float*)(val_tmp);
+        in[i/4][1] =*(float*)(val_tmp);
+        val_tmp=val_tmp+4;
+    }
+
+    p = fftw_plan_dft_1d(N,in,out,FFTW_FORWARD,FFTW_ESTIMATE);
+
+    fftw_execute(p);
+
+    for(int i=0;i<N;i++)
+	{
+		ofout<<out[i][0]<<std::endl;
+	}
+
+    fftw_destroy_plan(p);
+    fftw_free(in);
+    fftw_free(out);
+
+}
