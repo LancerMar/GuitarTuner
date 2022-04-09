7ad6e4a7fe6de7fa639f9e293e1a8c7db3fcb1a9
LancerMar <602545140@qq.com>
2022-04-09 21:02:27 +0100

    unit test restructure

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,22 @@
-#define BOOST_TEST_MODULE TSTTests
+#define BOOST_TEST_MODULE UnitTests
 #include <boost/test/unit_test.hpp>
 
 #include <iostream>
 
-#include "test_src.h"
+#include "i2s_mems_mic.h"
 
-BOOST_AUTO_TEST_CASE(TstTEST)
+BOOST_AUTO_TEST_CASE(OPEN_PCM_TEST)
 {
-    test tst;
-    std::cout<<"test==========="<<std::endl;
-    BOOST_CHECK_EQUAL(1, tst.test001());
+    I2Smic mic;
+    mic.open_pcm();
+    BOOST_CHECK_EQUAL(0, mic.get_rc());
+}
+
+BOOST_AUTO_TEST_CASE(PCM_SET_PARAMS_TEST)
+{
+    I2Smic mic;
+    mic.open_pcm();
+    mic.set_params();
+    //std::cout<<"rc_result: "<<mic.get_rc()<<std::endl;
+    BOOST_CHECK_EQUAL(0, mic.get_rc());
 }
